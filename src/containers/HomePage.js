fb5fb0030bb7e0251fa49a1bee1cc767c95abfc7
Jashan Devgan <jashandevgan@gmail.com>
2021-02-02 00:31:54 +0530

    Added Responsive NavBar
    Table to be inserted in HomePage, Provided routing for the side menu options (Notifications, Profile)

---
+++ src/containers/HomePage.js
@@ -0,0 +1,11 @@
+import Typography from '@material-ui/core/Typography';
+
+//insert Table in this component
+
+const HomePage = () => {
+    return (
+        <Typography variant="h1">Homepage of the app</Typography>
+    );
+}
+
+export default HomePage;
