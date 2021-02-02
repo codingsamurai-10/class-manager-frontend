cb3d2dde2822c247c19ab74541c4722588f6db6a
Jashan Devgan <jashandevgan@gmail.com>
2021-02-02 15:08:16 +0530

    Routing Not working on click in side menu
    ->Added side menu collapsible profile option
    ->routing to be fixed

---
+++ src/containers/Notifications.js
@@ -0,0 +1,11 @@
+import { Typography } from "@material-ui/core";
+
+const Notifications = () => {
+    return ( 
+        <div className="notifications">
+            <Typography variant = 'h2'>Notif Page</Typography>
+        </div>
+     );
+}
+ 
+export default Notifications;
