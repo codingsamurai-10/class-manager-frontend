f58006ce37796034fbd1293650dd0b21d1e6762a
Jashan Devgan <jashandevgan@gmail.com>
2021-02-05 13:53:01 +0530

    Added Routing

--- src/Routing.js
+++ src/Routing.js
@@ -1,20 +1,17 @@
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './containers/HomePage';
 import Notifications from './containers/Notifications';
-import Profile from './containers/Profile';
+
 
 
 
 
 const Routing = () => {
     return (
-        <div>
             <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route exact path='/Notifications' component={Notifications} />
-                <Route exact path='/Profile' component={Profile} />
             </Switch>
-        </div>
     );
 }
 
--- src/containers/Profile.js
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@material-ui/core";
-
-const Profile = () => {
-    return (
-        <Typography variant='h2'>Profile Page</Typography>
-    );
-}
-
-export default Profile;
