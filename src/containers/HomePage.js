535a7862901291964b200f2a94d832d672e8ae90
Aditya Rana <official.adityarana01@gmail.com>
2021-03-19 16:24:38 +0530

    Added reschedule and cancel buttons that open respective dialog boxes

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,9 +1,11 @@
 // import Typography from '@material-ui/core/Typography';
 import TimeTableContainer from './TimeTableContainer';
+import SlotChangingOptions from './SlotChangingOptions';
 
 const HomePage = () => {
     return (
         <div>
+            <SlotChangingOptions />
             <TimeTableContainer />
         </div>
     );
