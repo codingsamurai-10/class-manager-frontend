a04a65bd65866cab793489f79f86b2c117536bd8
Aditya Rana <official.adityarana01@gmail.com>
2021-03-26 18:20:59 +0530

    Renamed component

--- src/containers/SlotChangingOptions.js
+++ src/containers/SlotChangingOptions.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import RescheduleDialogBox from './BookDialogBox';
+import BookDialogBox from './BookDialogBox';
 import CancelDialogBox from './CancelDialogBox';
 
 const useStyles = makeStyles((theme) => ({
@@ -17,7 +17,7 @@ export default function SlotChangingOptions() {
   return (
     <div className={classes.root}>
       <CancelDialogBox />
-      <RescheduleDialogBox />
+      <BookDialogBox />
     </div>
   )
 }
