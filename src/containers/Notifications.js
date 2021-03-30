bb0826ebf137acde6b6be75850d4ab5a8c4f5367
Jashan Devgan <jashandevgan@gmail.com>
2021-03-30 13:52:19 +0530

    Added changes json data for notification page

--- src/containers/Notifications.js
+++ src/containers/Notifications.js
@@ -1,55 +1,18 @@
 import { Avatar, Divider, List, ListItem, ListItemAvatar, ListItemText } from "@material-ui/core";
-import React from "react";
+import {useState, useEffect} from "react";
+
 
 //Data in form of json will be used in this same structure.
-let notifs = [
-  {
-    subject: "Technical Communication",
-    cancelled: false,
-    prevTime: "12:00",
-    prevDate: "18/03/2021",
-    newDate: "20/03/2021",
-    newTime: "3:00"
-  },
-  {
-    subject: "Machines 2",
-    cancelled: false,
-    prevTime: "13:00",
-    prevDate: "18/03/2021",
-    newDate: "20/03/2021",
-    newTime: "4:00"
-  },
-  {
-    subject: "Microprocessors",
-    cancelled: true,
-    prevTime: "8:00",
-    prevDate: "18/03/2021",
-    newDate: "",
-    newTime: ""
-  },
-  {
-    subject: "Machine Learning",
-    cancelled: false,
-    prevTime: "9:00",
-    prevDate: "20/03/2021",
-    newDate: "21/03/2021",
-    newTime: "10:00"
-  },
-  {
-    subject: "Control Systems",
-    cancelled: true,
-    prevTime: "9:00",
-    prevDate: "20/03/2021",
-    newDate: "",
-    newTime: ""
-  },
-  
-]
-
-const Notifications = () => { //data fetching to be done in different file and passed as props to this component.
+const Notifications = () => {
+  //data fetching to be done in different file and passed as props to this component.
+  const [notifs, setNotifs] = useState(null);
+  const getItems = () => fetch("http://localhost:3001/changes").then(res => res.json());
+  useEffect(() => {
+    getItems().then(data => setNotifs(data));
+  }, []);
   return (
     <List className='notifications'>
-      {
+      {notifs &&
         notifs.map((notif, index) => ( //use stable keys instead of index
           <>
             <ListItem key={index} button alignItems="flex-start">
